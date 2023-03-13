69e537fa7324d1ad781ca011ccf2140bdc9f5ea7
jpcordeiro10 <58052261+jpcordeiro10@users.noreply.github.com>
2023-03-13 17:12:19 -0300

    Add files via upload

---
+++ EspCaminhao.c
@@ -0,0 +1,336 @@
+// Firmware da ESP do caminhao responsavel por:
+// - Ler a temperatura
+// - Receber dados do gps
+// - Recebimento do nível do tanque e da temperatura do tanque 
+// - Gravar tudo no cartao SD
+
+/* --- Inclusao de bibliotecas --- */
+#include <esp_now.h>
+#include <WiFi.h>
+// Temperatura
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// GPS
+#include <TinyGPSPlus.h>
+// SD Card
+#include <SD.h>
+#include <SPI.h>
+/* ------------------------------- */
+
+/* --- Pinos --- */ 
+// Numero de leituras de temperatura
+#define NUMERODEMEDIDAS 10
+// Temperatura
+#define oneWireBus 22  
+// GPS
+#define PIN_SPI_SD_CS          5
+#define PIN_SPI_SD_MOSI       23
+#define PIN_SPI_SD_MISO       19
+#define PIN_SPI_SD_CLK        18
+SPIClass sd_spi = SPIClass(3);
+/* ------------------------------- */
+/* ---- Operadores Logicos */
+#define true 1
+#define false 0
+int DadosRecebidos = false;
+/* --- Objetos das bibliotecas --- */
+// Temperatura    
+    OneWire oneWire(oneWireBus);
+    DallasTemperature sensors(&oneWire);
+// GPS
+    TinyGPSPlus gps;
+// SD Card
+    File myFile;
+/* ------------------------------- */
+
+/* --- Variaveis --- */
+// Variaveis para Leitura da Temperatura
+float Acumulador = 0, TemperaturaLida, TempCalibrada, MediaTemp, CorrecaoTemp;
+// Variavel de recebimento
+float TemperaturaCaminhao, TemperaturaTanque, Volume;
+// P/ GPS
+unsigned long lastDisplayTime = 0;
+/* ------------------------------- */
+
+ // Struct que utilizada para gravar no arquivo dentro do cartao SD
+typedef struct Dados SENSORES;
+struct Dados{
+  float latitude;
+  float longitude;
+  float altura;
+  float volume_tanque;
+  float temperatura_tanque;
+  float temperatura_caminhao;
+  float velocidade; 
+  int numero_de_satelites;
+  int dia, mes, ano;
+};
+SENSORES dados;
+/* --- Struct para envio --- */
+typedef struct struct_message {
+    float Temperatura;
+    float Volume;
+} struct_message;
+struct_message Readings;
+/* --- Struct para recebimento --- */
+typedef struct struct_received {
+    float Temperatura;
+    float Volume;
+} struct_received;
+struct_received Recebimento;
+/* ------------------------------- */
+// Endereço MAC da ESP que ira receber
+// EC:62:60:9B:98:24
+uint8_t broadcastAddress[] = {0xEC,0x62,0x60,0x9B,0x98,0x24}; 
+
+/* --- Funcoes --- */ 
+// Inicializa cartão sd
+void InicializaCartao(){ // Tem que trocar o numero dentro do SD begin de acordo com o pino CS do microcontrolador
+    Serial.print("Initializing SD card...");
+    if (!SD.begin(PIN_SPI_SD_CS)) {
+        Serial.println("initialization failed!");
+    while (!SD.begin(PIN_SPI_SD_CS));
+    }
+    Serial.println("initialization done.");
+    File myFile = SD.open("/teste.csv");
+  
+    if(!myFile) {
+      Serial.println("File doesn't exist");
+      Serial.println("Creating file...");
+      myFile = SD.open("/teste.csv", FILE_WRITE);
+      myFile.print("Latitude");
+      myFile.print(",");
+      myFile.print("Longitude");
+      myFile.print(",");
+      myFile.print("Altitude");
+      myFile.print(",");
+      myFile.print("Ano");
+      myFile.print(",");
+      myFile.print("Mes");
+      myFile.print(",");
+      myFile.print("Dia");
+      myFile.print(",");
+      myFile.print("Velocidade(km/h)");
+      myFile.print(",");
+      myFile.print("Satelites");
+      myFile.print(",");
+      myFile.print("Volume(L)");
+      myFile.print(",");
+      myFile.print("TemperaturaCaminhao");
+      myFile.print(",");
+      myFile.println("TemperaturaTanque");
+    }
+    else {
+      Serial.println("File already exists");  
+    }
+    myFile.close();
+}
+/* void InicializaEspNow(){ // Funcao responsavel por inicializar o EspNow
+    WiFi.mode(WIFI_STA);
+    Serial.println();
+    Serial.println(WiFi.macAddress());
+    
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+    // register peer
+    esp_now_peer_info_t peerInfo;
+
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+            
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+    }
+} */
+// Leitura do modulo GPS
+void LeituraGPS(){ // Funcao que realiza a leitura da string de dados obtida pelo modulo GPS e grava na Struct
+    Serial.println("GPS");
+//    delay(2000);
+    if (Serial2.available() > 0){
+        gps.encode(Serial2.read());
+        while (gps.location.isValid()&& (millis() - lastDisplayTime >= 2000)){
+            // mostra a latitude
+            Serial.print("Latitude= "); 
+            dados.latitude = gps.location.lat();
+            Serial.println(dados.latitude, 6); 
+            
+            // mostra a longitude
+            Serial.print("Longitude= ");
+            dados.longitude = gps.location.lng(); 
+            Serial.println(dados.longitude, 6); 
+            
+            // Altitude em metros 
+            Serial.print("Altitude in meters = "); 
+            dados.altura = gps.altitude.meters();
+            Serial.println(dados.altura); 
+
+            // ano (2000+) 
+            Serial.print("Year = "); 
+            dados.ano =  gps.date.year();
+            Serial.println(dados.ano); 
+            
+            // mes (1-12)
+            Serial.print("Month = "); 
+            dados.mes = gps.date.month();
+            Serial.println(dados.mes); 
+            
+            // dia (1-31) 
+            Serial.print("Day = "); 
+            dados.dia = gps.date.day();
+            Serial.println(dados.dia); 
+
+            // velocidade em km/h 
+            Serial.print("Speed in km/h = "); 
+            dados.velocidade = gps.speed.kmph();
+            Serial.println(dados.velocidade); 
+        
+            // número de satélites em uso
+            Serial.print("Number os satellites in use = ");
+            dados.numero_de_satelites = gps.satellites.value(); 
+            Serial.println(dados.numero_de_satelites);
+            
+            // volume do tanque
+            Serial.print("Volume em Litros [L] = ");
+            Serial.println(dados.volume_tanque);
+
+            // temperatura do leite tanque
+            Serial.print("Temperatura do leite [°C] = ");
+            Serial.println(dados.temperatura_tanque);
+
+            // temperatura do leite caminhao
+            Serial.print("Temperatura do leite [°C] = ");
+            Serial.println(dados.temperatura_caminhao);
+            lastDisplayTime = millis();
+            LeituraTemperatura();
+            GravacaoDeDados();
+        }
+    }              
+}
+void LeituraTemperatura(){ // Faz a leitura e a correcao da temperatura
+    sensors.requestTemperatures();
+    sensors.getTempCByIndex(0);
+
+    int i;
+    for (i = 0; i <= NUMERODEMEDIDAS; i++){ // Pode alterar as medidas no define
+        TemperaturaLida = sensors.getTempCByIndex(0);
+        Acumulador = TemperaturaLida + Acumulador;
+    }
+    MediaTemp = Acumulador / 10;
+    Readings.Temperatura = MediaTemp;
+    TemperaturaCaminhao = MediaTemp;
+    dados.temperatura_caminhao = TemperaturaCaminhao;
+    Serial.print("Temperatura Lida [L]: ");
+    Serial.println(TemperaturaLida);   
+    Serial.print("Temperatura Media [L]: ");
+    Serial.println(TemperaturaCaminhao);   
+}
+
+// Gravacao de dados no cartao SD
+void GravacaoDeDados(){ // Funcao que realiza a gravacao da Struct dentro de um arquivo .csv dentro do cartao SD
+    // Note que os dados são separados por virgulas e a cada Enter "println" marca o inicio de uma nova sentenca obtida
+    // Isso facilita a tabulação de um .csv dentro do Excel
+    Serial.println("Gravação");
+    myFile = SD.open("/teste.csv", FILE_APPEND);
+        if (myFile)
+            {
+            myFile.print(dados.latitude);
+            myFile.print(",");
+            myFile.print(dados.longitude);
+            myFile.print(",");
+            myFile.print(dados.altura);
+            myFile.print(",");
+            myFile.print(dados.ano);
+            myFile.print(",");
+            myFile.print(dados.mes);
+            myFile.print(",");
+            myFile.print(dados.dia);
+            myFile.print(",");
+            myFile.print(dados.velocidade);
+            myFile.print(",");
+            myFile.println(dados.numero_de_satelites);
+            myFile.print(",");
+            myFile.print(dados.volume_tanque);
+            myFile.print(",");
+            myFile.print(CorrecaoTemp);
+            myFile.print(",");
+            myFile.println(TemperaturaTanque);
+            
+            Serial.println("Gravou");
+            }
+    myFile.close();
+    DadosRecebidos = false;
+}
+
+void onReceiveData(const uint8_t *mac, const uint8_t *data, int len) {
+ 
+  Serial.print("Received from MAC: "); 
+  for (int i = 0; i < 6; i++) {
+    Serial.printf("%02X", mac[i]);
+    if (i < 5)Serial.print(":");
+  } // Printa o endereco MAC recebido do hardware 
+  Serial.print("\n");
+  
+  memcpy(&Recebimento, data, sizeof(Recebimento));
+  TemperaturaTanque = Recebimento.Temperatura; // Recebe a temperatura do caminhao
+  Volume = Recebimento.Volume;
+  CorrecaoTemp = TemperaturaCaminhao - TemperaturaTanque;
+  dados.temperatura_tanque = TemperaturaTanque;  
+  dados.volume_tanque = Volume;
+  DadosRecebidos = true;
+}
+void setup (){
+    // UART com o monitor serial do PC
+    Serial.begin(115200);
+    // UART com o modulo GPS
+    Serial2.begin(9600);
+    sensors.begin();
+    WiFi.mode(WIFI_STA);
+    Serial.println();
+    Serial.println(WiFi.macAddress());
+    
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+    // register peer
+    esp_now_peer_info_t peerInfo;
+
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+            
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+    }
+    InicializaCartao();
+   /*   InicializaEspNow();
+*/
+}
+
+void loop(){
+  Serial.println(DadosRecebidos);
+    if(DadosRecebidos == true){
+        LeituraGPS();
+        }
+
+    /* --- Envio das leituras --- */
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Readings, sizeof(Readings));
+    if (result == ESP_OK) {
+    Serial.println("Sent with success");
+    } else {
+    Serial.println("Error sending the data");
+    }
+    /* ------------------------ */
+    /* --- Recebimento ------- */
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+    }
+    esp_now_register_recv_cb(onReceiveData);
+    /* ---------------------- */
+}
---
+++ EspTanque.c
@@ -0,0 +1,209 @@
+// Firmware da ESP do tanque responsavel por:
+// - Leitura da distancia do sensor ultrassonico
+// - Calculo do nivel do tanque
+// - Leitura da temperatura com a calibracao
+// - Uso do ESPNOW para envio
+// - Envio da média da temperatura e do nível do tanque [Wireless]
+
+/* --- Inclusao de bibliotecas --- */
+// Envio [ESPNOW]
+#include <esp_now.h>
+#include <WiFi.h>
+// Temperatura
+#include <OneWire.h>
+#include <DallasTemperature.h>
+// Sensor de nivel 
+#include <Adafruit_VL53L0X.h>
+#include <Wire.h>
+#include <math.h>
+
+/* ------------------------------- */
+/* --- Pinos --- */
+// Temperatura
+#define oneWireBus 22
+
+// SENSOR DE NIVEL (LASER)
+#define I2C_SDA 33 
+#define I2C_SCL 32
+/* ------------------------------- */
+
+/* --- Constantes --- */
+#define NUMERODEMEDIDAS 10
+#define PI_APROXIMADO 3.1415927
+#define DIAMETRO     10.8 //[cm]   valor utilizado para teste, valor para o modelo de caminhão escolhido ----> 200 [cm] 
+#define ALTURA_TANQUE  20.5  //[cm]  valor utilizado para teste, valor para o modelo de caminhão escolhido ----> 250 [cm]
+/* ------------------------------- */
+
+/* --- Objetos das bibliotecas --- */
+// Temperatura    
+    OneWire oneWire(oneWireBus);
+    DallasTemperature sensors(&oneWire);
+// Sensor a laser
+    Adafruit_VL53L0X lox = Adafruit_VL53L0X();  
+/* ------------------------------- */
+
+/* --- Variaveis --- */
+// Endereço MAC
+uint8_t broadcastAddress[] = {0x08, 0x3A,0xF2,0xAB,0xAC,0x70}; // Endereço MAC da ESP que ira receber
+// Variaveis para Leitura da Temperatura
+float Acumulador = 0, TemperaturaLida, TempCalibrada, MediaTemp, CorrecaoTemp;
+// Variavel de recebimento
+float TemperaturaCaminhao, TemperaturaTanque;
+// Variaveis para calculo do nivel 
+float r = DIAMETRO/2, h;
+float distancia_laser;
+float area_tanque;
+float distancia;
+float Volume; 
+/* --- Struct para envio --- */
+typedef struct struct_message {
+    float Temperatura;
+    float Volume;
+} struct_message;
+struct_message Readings;
+/* --- Struct para recebimento --- */
+typedef struct struct_received {
+    float Temperatura;
+    float Volume;
+} struct_received;
+struct_received Recebimento;
+
+
+
+void InicializaLaser(){ // Funcao para inicialização do sensor a laser
+    Serial.println("Adafruit VL53L0X test");
+    if (!lox.begin()){ // Teste do sensor a laser aguarda enquanto não ler
+    Serial.println(F("Failed to boot VL53L0X"));
+    while(1);
+    }
+}
+void InicializaEspNow(){ // Funcao responsavel por inicializar o EspNow
+//    WiFi.mode(WIFI_STA);
+//    Serial.println();
+//    Serial.println(WiFi.macAddress());
+//    
+//    if (esp_now_init() != ESP_OK) {
+//    Serial.println("Error initializing ESP-NOW");
+//    return;
+//  }
+//    // register peer
+//    esp_now_peer_info_t peerInfo;
+//
+//    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+//    peerInfo.channel = 0;  
+//    peerInfo.encrypt = false;
+//            
+//    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+//    Serial.println("Failed to add peer");
+//    return;
+//    }
+}
+void LeituraLaser(){ // Leitura do sensor a laser
+    VL53L0X_RangingMeasurementData_t measure;
+
+    lox.rangingTest(&measure, false);
+    
+    if (measure.RangeStatus != 4) {
+      distancia_laser = measure.RangeMilliMeter;
+      Serial.print("Leitura laser(mm): ");
+      Serial.println(distancia_laser);
+     } else {
+      Serial.println("out of range");
+  }
+}
+void CalculoVolumeTanque(){
+    distancia = distancia_laser/10;
+        
+    h = ALTURA_TANQUE - distancia; // altura do líquido
+    area_tanque = ((PI_APROXIMADO * (r*r))/2);
+    Readings.Volume = Volume;
+    Volume = ((h * area_tanque)/1000); // volume final calculado em litros [L]
+    Serial.print("Volume(L): ");
+    Serial.println(Volume);
+}
+void LeituraTemperatura(){ // Faz a leitura e a correcao da temperatura
+    sensors.requestTemperatures();
+    sensors.getTempCByIndex(0);
+
+    int i;
+    for (i = 0; i <= NUMERODEMEDIDAS; i++){ // Pode alterar as medidas no define
+        TemperaturaLida = sensors.getTempCByIndex(0);
+        Acumulador = TemperaturaLida + Acumulador;
+        Serial.print("Temperatura: ");
+        Serial.print(TemperaturaLida);
+        delay(300);
+    }
+    MediaTemp = Acumulador / 10;
+    Readings.Temperatura = MediaTemp;
+    TemperaturaTanque = MediaTemp;
+    Serial.print("Temperatura: ");
+    Serial.print(TemperaturaTanque);   
+    
+}
+// Funcao abaixo realiza o recebimento dos dados
+void onReceiveData(const uint8_t *mac, const uint8_t *data, int len) {
+  Serial.print("Received from MAC: "); 
+  for (int i = 0; i < 6; i++) {
+    Serial.printf("%02X", mac[i]);
+    if (i < 5)Serial.print(":");
+  } // Printa o endereco MAC recebido do hardware 
+  Serial.print("\n");
+  
+  memcpy(&Recebimento, data, sizeof(Recebimento));
+  TemperaturaCaminhao = Recebimento.Temperatura; // Recebe a temperatura do caminhao
+}
+void setup(){
+    Serial.begin(115200); // Inicia a Serial
+    Wire.begin(I2C_SDA, I2C_SCL); // Inicia o protocolo I2C
+    sensors.begin(); // Inicializa a leitura da temperatura
+    InicializaLaser();
+    
+    WiFi.mode(WIFI_STA);
+    Serial.println();
+    Serial.println(WiFi.macAddress());
+    
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+  }
+    // register peer
+    esp_now_peer_info_t peerInfo;
+
+    memcpy(peerInfo.peer_addr, broadcastAddress, 6);
+    peerInfo.channel = 0;  
+    peerInfo.encrypt = false;
+            
+    if (esp_now_add_peer(&peerInfo) != ESP_OK){
+    Serial.println("Failed to add peer");
+    return;
+    }
+
+    
+//    InicializaEspNow();
+    
+}
+
+void loop(){
+    /* --- leitura dos sensores --- */
+    LeituraLaser();
+    CalculoVolumeTanque();
+    LeituraTemperatura();
+    /* --------------------------  */
+    /* --- Envio das leituras --- */
+    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &Readings, sizeof(Readings));
+    if (result == ESP_OK) {
+    Serial.println("Sent with success");
+    } else {
+    Serial.println("Error sending the data");
+    }
+    /* ------------------------ */
+    /* --- Recebimento ------- */
+    if (esp_now_init() != ESP_OK) {
+    Serial.println("Error initializing ESP-NOW");
+    return;
+    }
+    esp_now_register_recv_cb(onReceiveData);
+    /* ---------------------- */
+//    delay(500);
+
+}
